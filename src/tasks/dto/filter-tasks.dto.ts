90ba7f51db015a33385db2ea12bf96846a45a66f
AASysoev <Chaegh2Oof4phok3yaph>
2020-09-12 01:54:54 +0300

    Handling errors

--- src/tasks/dto/filter-tasks.dto.ts
+++ src/tasks/dto/filter-tasks.dto.ts
@@ -1,6 +1,6 @@
 import { TaskStatus } from '../task.model';
 
-export interface FilterTasksDto {
+export class FilterTasksDto {
     status: TaskStatus;
     search: string;
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { uuid } from 'uuidv4';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -16,7 +16,13 @@ export class TasksService {
     }
 
     public getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+
+        if (!found) {
+            throw new NotFoundException(`Task with ID "${id}" is not found`);
+        }
+
+        return found;
     }
 
     public createTask({ title, description }: CreateTaskDto): Task {
@@ -33,7 +39,7 @@ export class TasksService {
     }
 
     public patchTask(id: string, status: TaskStatus): Task {
-        const task: Task = this.tasks.find(task => task.id === id);
+        const task: Task = this.getTaskById(id);
 
         task.status = status;
 
@@ -41,7 +47,8 @@ export class TasksService {
     }
 
     public deleteTask(id: string): void {
-        const index: number = this.tasks.indexOf(task => task.id === id);
+        const found = this.getTaskById(id);
+        const index: number = this.tasks.indexOf(task => task.id === found.id);
 
         this.tasks.splice(index, 1);
     }
