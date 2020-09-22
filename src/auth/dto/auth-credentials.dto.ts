4d04b1651ace7881d233416d9bae1eecf6761bfd
AASysoev <Chaegh2Oof4phok3yaph>
2020-09-22 16:21:48 +0300

    add sign up endpoint

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,4 @@
+export class AuthCredentialsDto {
+    public username: string;
+    public password: string;
+}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,15 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+    public async signUp({ username, password }: AuthCredentialsDto): Promise<void> {
+        const user: User = new User();
 
+        user.username = username;
+        user.password = password;
+
+        await user.save();
+    }
 }
