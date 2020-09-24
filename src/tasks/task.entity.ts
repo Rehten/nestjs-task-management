62a0e4ab3b38e775759269c577edbafe44330917
AASysoev <Chaegh2Oof4phok3yaph>
2020-09-25 00:36:40 +0300

    add databse relation

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task.status';
+import { User } from '../auth/user.entity';
 
 @Entity()
 export class Task extends BaseEntity {
@@ -14,4 +15,7 @@ export class Task extends BaseEntity {
 
     @Column()
     public status: TaskStatus;
+
+    @ManyToOne(type => User, user => user.tasks, { eager: false })
+    public user: User;
 }
